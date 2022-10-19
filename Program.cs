a04b6dc24b64c894d52bb3033590410b01067c1c
chris martinex <uschrismartinez@gmail.com>
2022-10-19 13:55:31 -0700

    simplify data validation

--- Program.cs
+++ Program.cs
@@ -2,33 +2,39 @@
 // GitHub Challenge ADD TWO NUMBERS
 // 10/18/22
 
-string numberOne ="";
-string numeroTwo ="";
-int wht = 0;
+string numberOne = "";
+string numeroTwo = "";
+int Num1 = 0;
+int Num2 = 0;
 
-Console.Clear();Console.WriteLine ("lets add two numbers");
+Console.Clear(); Console.WriteLine("lets add two numbers");
 
- bool validation = true; 
- while (validation == true)
+bool validation = true;
+while (validation == true)
 {
-Console.WriteLine("Enter44 First Number"); numberOne = Console.ReadLine();
-bool num1 = Int32.TryParse (numberOne, out wht);
-if (num1 == true)
-{
- validation = false;
-}}
- validation = true;
+ Console.WriteLine("Enter First Number"); 
+ numberOne = Console.ReadLine();
+ //numberone = "dog";
+//    num1 =  false           "dog"             
+ bool num1 = Int32.TryParse(numberOne, out Num1);
+ if (num1 == true)
+ {
+  validation = false;
+ }
+}
+validation = true;
 
- while(validation == true){
+while (validation == true)
+{
  Console.WriteLine("Enter Second Number"); numeroTwo = Console.ReadLine();
-bool num2 = Int32.TryParse (numeroTwo, out wht);
-if (num2 == true)
-{
- validation = false;
+ bool num2 = Int32.TryParse(numeroTwo, out Num2);
+ if (num2 == true)
+ {
+  validation = false;
+ }
 }
-}
- int Num1 = Convert.ToInt32(numberOne);
-  int Num2 = Convert.ToInt32(numeroTwo);
+// int Num1 = Convert.ToInt32(numberOne);
+// int Num2 = Convert.ToInt32(numeroTwo);
 
 
 Console.WriteLine("Your sum is:" + (Num1 + Num2));
